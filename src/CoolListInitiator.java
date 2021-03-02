8775c6411a2bef33bbf59654645a862afe3e3a28
Benjamin Hughes <behu@kea.dk>
2021-03-02 14:18:50 +0100

    asd

---
+++ src/CoolListInitiator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class CoolListInitiator {
+    public static void main(String[] args) {
+        CoolList cool = new CoolList();
+        // The add method i have written
+        cool.add("First item!!");
+        // The get method i have written
+        System.out.println(cool.get(0));
+    }
+}
